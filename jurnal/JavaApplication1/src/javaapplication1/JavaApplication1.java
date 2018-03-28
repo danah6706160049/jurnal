1ef2c65f483327f03f19fb85c1e8ef975c889ce9
Rahmad Satria Kurniawan <37843980+satriakurniawan@users.noreply.github.com>
2018-03-28 12:06:02 +0700

    udah diganti
    sssss

--- jurnal/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ jurnal/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -6,6 +6,7 @@
 package javaapplication1;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 /**
  *
@@ -13,22 +14,47 @@ import java.util.Scanner;
  */
 public class JavaApplication1 {
 
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int a = 0;
-        int b = 1;
-        int hasil = 0;
-        
-        System.out.print("Input : ");
-        int input = scn.nextInt();
-        
-        for (int i = 1; i <= input; i++) {
-            a = b;
-            b = hasil;
-            System.out.print(hasil+" ");
-            hasil = a + b;
+    /**
+     * @param args the command line arguments
+     */
+   Scanner scan;
+    int a1, b1, c1, a2, b2, c2, limit, i;
+    public JavaApplication1() {
+        scan = new Scanner(System.in);
+        a1 = c1 = 1;
+        b1 = a2 = b2 = c2 = limit = i = 0;
+    }
+    public void input() {
+        try {
+            System.out.printf("Enter limit: ");
+            limit = scan.nextInt();
+            scan.close();
+        } catch (InputMismatchException | NumberFormatException e) {
+            System.err.println("Error Occurred!");
+            System.exit(0);
         }
-        
+    }
+    public void compute() {
+        for (; i < limit; i++) {
+            a1 = b1;
+            b1 = c1;
+            c1 = a1 + b1;
+            a2 = c1;
+            c2 = b1;
+        }
+        System.out.println("Series in Reverse Order:");
+        System.out.println(a2 + "\n" + c2);
+        for (i = limit - 1; i >= 0; i--) {
+            b2 = a2;
+            a2 = c2;
+            c2 = b2 - a2;
+            System.out.println(c2);
+        }
+    }
+    public static void main(String[] args) {
+        JavaApplication1 fr = new JavaApplication1();
+        fr.input();
+        fr.compute();
     }
     
 }
